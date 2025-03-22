a73ecddef7870c62ab85494a99ef37f18e946aa0
Mikhail <mikhailzvnv@gmail.com>
2025-03-22 17:18:37 +0300

    edit firebase config

--- src/configs/FirebaseConfig.js
+++ src/configs/FirebaseConfig.js
@@ -1,12 +1,10 @@
 const FirebaseConfig = {
-  apiKey: 'AIzaSyAOWoRQGoegkmwnCca7B2GG5GUG_ZP1lcM',
-  authDomain: 'emilus.firebaseapp.com',
-  databaseURL: 'https://emilus.firebaseio.com',
-  projectId: 'emilus',
-  storageBucket: 'emilus.appspot.com',
-  messagingSenderId: '807555350717',
-  appId: '1:807555350717:web:145ba7c21af47203a2f7d4',
-  measurementId: 'G-8KE7HJB252'
+  apiKey: "AIzaSyDLLazuGltFCqB8SD8oZN14-zrcGLKe69c",
+  authDomain: "test-task-1-4fc0b.firebaseapp.com",
+  projectId: "test-task-1-4fc0b",
+  storageBucket: "test-task-1-4fc0b.firebasestorage.app",
+  messagingSenderId: "1054261811743",
+  appId: "1:1054261811743:web:34f9c58c8c7e794023d32a"
 };
 
 export default FirebaseConfig
